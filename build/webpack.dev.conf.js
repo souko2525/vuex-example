a4ee7545c6b9c36fcaafac5f281ffcd6008a06cf
souko2525 <yusuke.h0808@gmail.com>
2018-01-14 01:59:29 +0900

    mod env

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,5 +1,4 @@
 'use strict'
-require('dotenv').config();
 
 const utils = require('./utils')
 const webpack = require('webpack')
@@ -12,7 +11,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 
-const HOST = process.env.HOST
+//const HOST = process.env.HOST
+const HOST = '0.0.0.0'
 const PORT = process.env.PORT && Number(process.env.PORT)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
