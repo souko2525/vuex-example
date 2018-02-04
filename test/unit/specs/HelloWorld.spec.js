f1b562c56cd1aa41f437a8339e095b03b74972ae
souko2525 <yusuke.h0808@gmail.com>
2018-02-04 20:13:28 +0900

    add require

--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
+import router from '@/router'
+import store from '@/store'
 import HelloWorld from '@/components/HelloWorld'
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
     const Constructor = Vue.extend(HelloWorld)
-    const vm = new Constructor().$mount()
+    const vm = new Constructor({router, store}).$mount()
     expect(vm.$el.querySelector('.hello h1').textContent)
     .toEqual('Welcome to Your Vue.js App')
   })
