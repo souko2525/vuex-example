95e82bfc81dd1252bb5615f9283b9139c2e4869e
souko2525 <yusuke.h0808@gmail.com>
2018-06-15 01:20:25 +0900

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/image/logo.png">
-    <router-view/>
+    <v-app>
+    <v-navigation-drawer
+      fixed
+      clipped
+      app
+      v-model="navBar"
+    >
+        <v-list dense class="pt-0">
+          <router-link to="/">
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon>dashboard</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>DASHBOARD</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </router-link>
+          <router-link to="/setting">
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon>settings</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>SETTING</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </router-link>
+        </v-list>
+      </v-navigation-drawer>
+      <v-toolbar app></v-toolbar>
+      <v-content>
+        <v-container fluid>
+          <img src="./assets/image/logo.png">
+          <router-view/>
+         </v-container>
+       </v-content>
+       <v-footer app></v-footer>
+     </v-app>        
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import App from './App'
 import router from './router'
 import store from './store'
+import '../node_modules/vuetify/dist/vuetify.min.css'
+import '../node_modules/material-design-icons-iconfont/dist/material-design-icons.css'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
